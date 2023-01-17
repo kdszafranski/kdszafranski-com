1ba612f5c9271dcbc8aff4ffb817249239123570
Kris Szafranski <kris@thoughtshelter.com>
2023-01-16 19:24:03 -0600

    added more images, reformatted skills section, working on making carousels better

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -1,30 +1,60 @@
+$(document).ready(start);
+
+// data
 const basePath = 'img/game-images/';
 const images = [
   { description: `A Druid's Duel`, path: 'add1.png' },
   { description: `A Druid's Duel 2`, path: 'add2.png' },
   { description: `Monsters of Myth`, path: 'mom-dead.png' },
   { description: `A Druid's Duel`, path: 'add3.png' },
+  { description: `Monsters of Myth Closeup`, path: 'mom-close-up.png' },
   { description: `A Druid's Duel`, path: 'add4.png' },
+  { description: `Edge of the Abyss`, path: 'eoa.png' },
 ];
 
-let current = 0;
-let intId = setInterval(nextImage, 4500);
-
+// globals
+let current = getRandomNumber();
 // timer
-function resetInterval() {
+let intId = setInterval(nextImage, 4500);
+// carousels
+let carousels = [];
+
+function start() {
+  console.log('dom');
+  carousels = $('.carousel');
+  // start it up
+  seedCarouselStart();
+  
+  $('.carousel').on('click', resetInterval);
+}
+
+
+function resetInterval(e) {
+    console.log('clicked car');
     clearInterval(intId);
     intId = setInterval(nextImage, 4500);
     // go to next image
-    nextImage();
+    nextImage($(this));
 }
 
-function nextImage() {
+function nextImage(target) {
   current++;
   if (current == images.length) {
     current = 0;
   }
   // set image path and desc
-  $('#games-carousel').attr('src', basePath + images[current].path);
-  $('#games-carousel').attr('alt', images[current].description);
+  target.attr('src', basePath + images[current].path);
+  target.attr('alt', images[current].description);
+}
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * images.length);
+}
+
+function seedCarouselStart() {
+  for(let car in carousels) {
+    $(car).attr('src', basePath + images[getRandomNumber()].path);
+    $(car).attr('alt', images[current].description);
+  }
 }
 
