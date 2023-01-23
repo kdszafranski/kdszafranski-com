a19b5bc9814b5f0f1a549bcccc9bef20427f8461
Kris Szafranski <kris@thoughtshelter.com>
2023-01-23 14:10:32 -0600

    moved logic up to body element, added start of gamewin check

--- server/public/js/game.js
+++ server/public/js/game.js
@@ -4,13 +4,14 @@ let draggedItem = null;
 
 function start() {
   $('.draggable').on('dragstart', handleDrag);
-  $('#dropzone').on('dragover', allowDrop);
-  $('#dropzone').on('drop', handleDrop);
+  $('body').on('dragover', allowDrop);
+  $('body').on('drop', handleDrop);
 }
 
 function handleDrag(event) {
   console.log('drag start', event);
   draggedItem = $(this);
+  draggedItem.addClass('dragging');
 }
 
 function allowDrop(event) {
@@ -19,11 +20,34 @@ function allowDrop(event) {
 
 function handleDrop(event) {
   event.preventDefault();
-  console.log('dropped', draggedItem.id);  
-  draggedItem.parent().remove(draggedItem);
-  // drop
-  $(this).append(draggedItem);
-  draggedItem.attr('draggable', false);
-  draggedItem.addClass('collected');
-  draggedItem.removeClass('draggable');
+  console.log('dropped', draggedItem.attr('id'));
+
+  if(event.target.id === 'dropzone') {      
+      // clean up old DOM parent
+      draggedItem.parent().remove(draggedItem);
+      
+      // rearrange DOM
+      $('#dropzone').append(draggedItem);
+      draggedItem.attr('draggable', false);
+      draggedItem.addClass('collected');
+      draggedItem.removeClass('draggable');
+      draggedItem.removeClass('dragging');
+
+      checkWin();
+  } else {
+    draggedItem.removeClass('dragging');
+   }
+
+  draggedItem = null;
+
+}
+
+function checkWin() {
+  let loot = $('#dropzone').children();
+  for(let item of loot) {
+    console.log('items ', item.id);
+    if(item.id === 'gem') {
+      console.log('got the gem');
+    }
+  }
 }
