5139e249181f72f1ca8aacdaf7c894914553e88a
Kris Szafranski <kris@thoughtshelter.com>
2023-05-31 09:46:42 -0500

    added hard-coded image assets and endpoint to retrieve them

--- server/server.js
+++ server/server.js
@@ -2,7 +2,22 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const assets = {
+    imagePath: "http://localhost:5000/img/game-images",
+    images: [
+        { description: `A Druid's Duel`, path: 'add1.png' },
+        { description: `Monsters of Myth`, path: 'mom-dead.png' },
+        { description: `A Druid's Duel`, path: 'add3.png' },
+        { description: `Monsters of Myth Closeup`, path: 'mom-close-up.png' },
+        { description: `A Druid's Duel`, path: 'add4.png' },
+    ],
+};
+
 app.use(express.static('server/public'));
+
+app.get("/assets", (req, res) => {
+    res.json(assets);
+});
 
 app.listen(PORT, () => {
     console.log(`App running on port: ${PORT}`);    
